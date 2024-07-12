3ebe7149559bc115a1637d2c6d385178a70e28dd
Manojkumar Patil <88649192+patilmanojkumar@users.noreply.github.com>
2024-07-12 13:11:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ def compute_cdvi(cv, adj_r_squared):
     return cdvi
 
 # Streamlit app
-st.title('Trend Analyser')
+st.title('Trend Analyser by [Manojkumar Patil](https://github.com/patilmanojkumar)')
 
 # File upload
 uploaded_file = st.file_uploader("Upload a CSV, XLSX, or XLS file", type=["csv", "xlsx", "xls"])
