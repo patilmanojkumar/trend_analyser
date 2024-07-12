5671553c0050d9a2fc68cc3f7cec0b4292b945b7
Manojkumar Patil <88649192+patilmanojkumar@users.noreply.github.com>
2024-07-12 15:04:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,8 @@ def render_svg(svg_file):
         html = f'<img src="data:image/svg+xml;base64,{b64}"/>'
         return html
 # Streamlit app
-st.markdown('##Trend Analyser by [Manojkumar Patil](https://github.com/patilmanojkumar)')
+st.set_page_config(layout="wide")
+st.title('Trend Analyser by [Manojkumar Patil](https://github.com/patilmanojkumar)')
 # Displaying the dynamic SVG banner
 st.markdown(
     """
